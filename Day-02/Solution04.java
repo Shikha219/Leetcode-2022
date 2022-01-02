2020dd32e0575233944db3bcd85e48096a91a41f
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2022-01-02 20:59:36 +0530

    Number of Good Pairs

---
+++ Day-02/Solution04.java
@@ -0,0 +1,16 @@
+/* https://leetcode.com/problems/number-of-good-pairs/ */
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                count += hm.get(nums[i]);
+                hm.put(nums[i],hm.get(nums[i])+1);
+            }
+            else
+                hm.put(nums[i],1);
+        }
+        return count;
+    }
+}
